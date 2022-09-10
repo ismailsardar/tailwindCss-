589d333d3992e611fc8ce0c1fc4fe4b84b494fa3
ismail <ismailsardar@gmail.com>
2022-09-10 22:54:29 +0600

    this is my first Tailwind CSS project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        '16': '16rem',
+      }
+    },
+  },
+  plugins: [],
+}
